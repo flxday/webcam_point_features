1d1573135decae01b75aad019dfcee66627215ec
felix <felix.torres86@gmail.com>
2016-10-27 17:04:19 +0200

    point features

---
+++ CMakeFiles/webcam_poit_features.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/webcam_poit_features.dir/src/point_features.cpp.o"
+  "webcam_poit_features.pdb"
+  "webcam_poit_features"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/webcam_poit_features.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
