f947a3b877045b7cabdcdbbe7b3565beb4c89d1f
felix <felix.torres86@gmail.com>
2016-10-31 11:38:47 +0100

    point features

--- src/point_features.cpp
+++ src/point_features.cpp
@@ -9,6 +9,11 @@
 #include <iostream>
 #include <cstdlib>
 
+// OpenCV image objects and ROIs
+
+  cv::Mat camSubimage;
+  cv::Rect roi;
+
 //consts
 const unsigned int MIN_NUM_FEATURES = 300; //minimum number of point fetaures
 
@@ -16,68 +21,79 @@ int main(int argc, char *argv[])
 {
     cv::VideoCapture camera; //OpenCV video capture object
     cv::Mat image; //OpenCV image object
-	int cam_id; //camera id . Associated to device number in /dev/videoX
-	cv::Ptr<cv::ORB> orb_detector = new cv::ORB(MIN_NUM_FEATURES); //ORB point feature detector
+    int cam_id; //camera id . Associated to device number in /dev/videoX
+    cv::Ptr<cv::ORB> orb_detector = new cv::ORB(MIN_NUM_FEATURES); //ORB point feature detector
     cv::vector<cv::KeyPoint> point_set; //set of point features
     cv::Ptr<cv::DescriptorExtractor> orb_descriptor; //ORB descriptor
     orb_descriptor = cv::DescriptorExtractor::create("ORB"); //init the descriptor
     cv::Mat descriptor_set; //set of descriptors, for each feature there is an associated descriptor 
-	
-	//check user args
-	switch(argc)
-	{
-		case 1: //no argument provided, so try /dev/video0
-			cam_id = 0;  
-			break; 
-		case 2: //an argument is provided. Get it and set cam_id
-			cam_id = atoi(argv[1]);
-			break; 
-		default: 
-			std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl; 
-			std::cout << "EXIT program." << std::endl; 
-			break; 
-	}
-	
-	//advertising to the user 
-	std::cout << "Opening video device " << cam_id << std::endl;
+
+
+//check user args
+    switch(argc)
+    {
+        case 1: //no argument provided, so try /dev/video0
+            cam_id = 0;  
+            break; 
+        case 2: //an argument is provided. Get it and set cam_id
+            cam_id = atoi(argv[1]);
+            break; 
+        default: 
+            std::cout << "Invalid number of arguments. Call program as: webcam_capture [video_device_id]. " << std::endl; 
+            std::cout << "EXIT program." << std::endl; 
+            break; 
+    }
+    
+    //advertising to the user 
+    std::cout << "Opening video device " << cam_id << std::endl;
 
     //open the video stream and make sure it's opened
     if( !camera.open(cam_id) ) 
-	{
+    {
         std::cout << "Error opening the camera. May be invalid device id. EXIT program." << std::endl;
         return -1;
     }
 
     //Process loop. Capture and point feature extraction. User can quit pressing a key
     while(1)
-	{
-		//Read image and check it. Blocking call up to a new image arrives from camera.
-        if(!camera.read(image)) 
-		{
+    {
+        //Read image and check it. Blocking call up to a new image arrives from camera.
+    if(!camera.read(image)) 
+    {
             std::cout << "No image" << std::endl;
             cv::waitKey();
         }
-        		
+
+    /* Central pixel */
+    //std::cout << "Central pixel is (" << image.cols/2 << "," << image.rows/2 << ")" << std::endl;
+
+     /* Create a ROI (region of interest) inside the original image */
+    roi = cv::Rect((image.cols/2)-150, (image.rows/2)-150, 150, 150);
+    camSubimage = image(roi);
+
+    /* Quick and easy transformation, just multiply the matrix */
+    camSubimage = camSubimage*5;
+                
     //**************** Find ORB point fetaures and descriptors ****************************
         
         //clear previous points
         point_set.clear(); 
         
         //detect point features
-        orb_detector->detect(image, point_set);
+        orb_detector->detect(camSubimage, point_set);
         
         //extract descriptors
-        orb_descriptor->compute(image,point_set,descriptor_set);
+        orb_descriptor->compute(camSubimage,point_set,descriptor_set);
         
         //draw points on the image
-        cv::drawKeypoints( image, point_set, image, 255, cv::DrawMatchesFlags::DEFAULT );      
+        cv::drawKeypoints( camSubimage, point_set, camSubimage, 255, cv::DrawMatchesFlags::DEFAULT );      
                 
     //********************************************************************
-		
+        
         //show image
         cv::imshow("Output Window", image);
 
-		//Waits 1 millisecond to check if a key has been pressed. If so, breaks the loop. Otherwise continues.
+        //Waits 1 millisecond to check if a key has been pressed. If so, breaks the loop. Otherwise continues.
         if(cv::waitKey(1) >= 0) break;
     }   
 }
